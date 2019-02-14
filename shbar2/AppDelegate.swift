a4049cd2972d5d14e74b51e7e27ac17c354a96ed
Rich Infante <rich@richinfante.com>
2019-02-14 18:33:11 -0500

    Initial Commit

---
+++ shbar2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  shbar2
+//
+//  Created by Rich Infante on 2/14/19.
+//  Copyright © 2019 Rich Infante. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
